dd0d49af31717a8a0b6a0104693adf578edc6ea7
Ralf Lici <ralf@mandelbit.com>
2025-05-05 17:56:14 +0200

    Fix compilation warnings
    Some harmless but persistent compilation warnings appear mostly on older
    kernels. This commit adjusts code structure and restores a few dropped
    `const` qualifiers to eliminate unnecessary compiler complaints.
    
    Reported-by: Gert Doering <gert@greenie.muc.de>
    Closes: https://github.com/OpenVPN/ovpn-backports/issues/7
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

--- compat-include/net/genetlink.h
+++ compat-include/net/genetlink.h
@@ -19,8 +19,11 @@
 #define genl_split_ops genl_ops
 #define split_ops ops
 #define n_split_ops n_ops
+
+#ifndef container_of_const
 #define container_of_const(ptr, type, member) \
 	container_of_safe((ptr), (type), (member))
+#endif /* container_of_const */
 
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 2, 0) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9, 3) */
 
@@ -35,5 +38,16 @@
 #endif /* GENL_REQ_ATTR_CHECK */
 
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0) */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0)
+
+static inline struct net *ovpn_genl_info_net(const struct genl_info *info)
+{
+	return read_pnet(&((struct genl_info *)(info))->_net);
+}
+
+#define genl_info_net ovpn_genl_info_net
+
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0) */
 
 #endif /* _NET_OVPN_NET_GENETLINK_H_ */
