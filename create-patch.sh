6937ff1e8825cd53b7c3c7f324925bcd8e1cd635
Ralf Lici <ralf@mandelbit.com>
2025-05-05 17:49:21 +0200

    Improve patch creation tool
    Allow `ORIGINAL_DIR` to be passed as a variable to `create-patch.sh`,
    specifying the directory where the original, unmodified ovpn source
    files reside.
    
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

--- create-patch.sh
+++ create-patch.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-ORIGINAL_DIR="net-next/drivers/net/ovpn"
+ORIGINAL_DIR=${ORIGINAL_DIR:-net-next/drivers/net/ovpn}
 MODIFIED_DIR="drivers/net/ovpn"
+
+if [[ $ORIGINAL_DIR =~ ^/ ]]; then
+	substr="${ORIGINAL_DIR:1}"
+	ESCAPED_ORIGINAL_DIR="${substr//\//\\\/}"
+else
+	ESCAPED_ORIGINAL_DIR="${ORIGINAL_DIR//\//\\\/}"
+fi
+ESCAPED_MODIFIED_DIR="${MODIFIED_DIR//\//\\\/}"
 
 rm -f temp.patch
 
@@ -12,4 +20,4 @@ for filepath in "$ORIGINAL_DIR"/*; do
     fi
 done
 
-sed -i 's/net-next\///' temp.patch
+sed -i "s/$ESCAPED_ORIGINAL_DIR/$ESCAPED_MODIFIED_DIR/" temp.patch
