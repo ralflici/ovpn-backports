3d358091ae571b42f845eb76367023d90524fb31
Ralf Lici <ralf@mandelbit.com>
2025-06-03 09:09:27 +0200

    Update ovpn-net-next to ab672ca
    ovpn-next-next now points to ab672ca98c1bfc87f4572ff372b25a881843e0e5:
      ovpn: tcp: avoid sleep in atomic context
    
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

--- compat-include/net/udp.h
+++ compat-include/net/udp.h
@@ -6,6 +6,24 @@
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0)
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
+
+#ifndef udp_test_and_clear_bit
+#define udp_test_and_clear_bit(nr, sk)		\
+	test_and_clear_bit(UDP_FLAGS_##nr, &udp_sk(sk)->udp_flags)
+#endif /* udp_test_and_clear_bit */
+
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
+static inline void ovpn_udp_encap_disable(void)
+{
+	static_branch_dec(&udp_encap_needed_key);
+}
+
+#define udp_encap_disable ovpn_udp_encap_disable
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0) */
+
 #if IS_ENABLED(CONFIG_IPV6)
 static inline void ovpn_udpv6_encap_disable(void)
 {
@@ -24,6 +42,34 @@ static inline void ovpn_udpv6_encap_disable(void)
 
 #define udpv6_encap_disable ovpn_udpv6_encap_disable
 #endif /* IS_ENABLED(CONFIG_IPV6) */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0)
+static DEFINE_SPINLOCK(udp_encap_enabled_lock);
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0) */
+
+static inline void ovpn_udp_tunnel_encap_disable(struct sock *sk)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0)
+	unsigned long irq_flags;
+	int old_encap_enabled;
+
+	spin_lock_irqsave(&udp_encap_enabled_lock, irq_flags);
+	old_encap_enabled = udp_sk(sk)->encap_enabled;
+	udp_sk(sk)->encap_enabled = 0;
+	spin_unlock_irqrestore(&udp_encap_enabled_lock, irq_flags);
+#else
+	if (!udp_test_and_clear_bit(ENCAP_ENABLED, sk))
+		return;
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0) */
+
+#if IS_ENABLED(CONFIG_IPV6)
+	if (READ_ONCE(sk->sk_family) == PF_INET6)
+		udpv6_encap_disable();
+#endif
+	udp_encap_disable();
+}
+
+#define udp_tunnel_encap_disable ovpn_udp_tunnel_encap_disable
 
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0) */
 
