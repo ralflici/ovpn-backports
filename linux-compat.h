06501cec8cc896ca68c4e85efaf109c11b484b03
Ralf Lici <ralf@mandelbit.com>
2025-10-07 17:07:03 +0200

    Limit ovpn_sendmsg_locked definition to v6.5–v6.16 range
    The unexported sendmsg_locked function is required only on kernels from
    v6.5.0 through v6.16.0. However, our current reimplementation is defined
    for all kernels older than v6.16.0, which leads to unused function
    warnings on versions prior to v6.5.0.
    
    This commit sets a lower bound of v6.5.0 for the definition, eliminating
    the warning and aligning the implementation with actual usage.
    
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

--- linux-compat.h
+++ linux-compat.h
@@ -60,7 +60,8 @@ enum {
 
 #endif /* IFLA_OVPN_MAX */
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 5, 0) && \
+	LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0)
 
 #include <net/sock.h>
 static int ovpn_sendmsg_locked(struct sock *sk, struct msghdr *msg)
@@ -77,7 +78,7 @@ static int ovpn_sendmsg_locked(struct sock *sk, struct msghdr *msg)
 	return sock->ops->sendmsg_locked(sk, msg, size);
 }
 
-#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0) */
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(6, 5, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 16, 0) */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 0)
 
