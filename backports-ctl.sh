fbf663964b59f870bcdee24ae229ee9bcd1ecd7b
Ralf Lici <ralf@mandelbit.com>
2026-03-23 16:48:22 +0100

    Update ovpn-net-next to ca7e993
    ovpn-net-next now points to ca7e99335aea7c5977683624ba319157a4603f96:
      net: fjes: Drop fjes_acpi_driver and rework initialization
    
    Changelog:
     - d3244af ovpn: consolidate crypto allocations in one chunk
     - 7b80d8a selftests: ovpn: add test for the FW mark feature
     - 367f4b1 selftests: ovpn: check asymmetric peer-id
     - 2e570a5 ovpn: add support for asymmetric peer IDs
     - 77de28c selftests: ovpn: add notification parsing and matching
     - c841b67 ovpn: notify userspace on client float event
     - 4a64805 ovpn: pktid: use bitops.h API
     - 7e7ca01 ovpn: use correct array size to parse nested attributes in
       ovpn_nl_key_swap_doit
     - a8e136b selftests: ovpn: allow compiling ovpn-cli.c with mbedtls3
     - 8341c98 net: remove addr_len argument of recvmsg() handlers
    
    Note: tests in ovpn-backports are curerntly broken (missing YNL_CLI).
    
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

--- backports-ctl.sh
+++ backports-ctl.sh
@@ -3,7 +3,7 @@
 set -e
 
 KERNEL_REPO_URL='https://github.com/OpenVPN/ovpn-net-next.git'
-KERNEL_COMMIT=${KERNEL_COMMIT:-'fd6dad4e1ae296b67b87291256878a58dad36c93'}
+KERNEL_COMMIT=${KERNEL_COMMIT:-'ca7e99335aea7c5977683624ba319157a4603f96'}
 KERNEL_DIR="$PWD/kernel"
 
 get_ovpn() {
