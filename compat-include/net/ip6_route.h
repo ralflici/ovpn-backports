ralflici/ovpn-backports

commit 6c4d515ace75ef107ddc7fedf2d6389e2d6c1d76

Update ovpn-net-next to 98878ed91b68
ovpn-next-next now points to 98878ed91b68a3150126fccef125ee7b1bb86ab2:
  Merge branch 'intel-wired-lan-updates-2024-04-30-ixgbe-i40e-ice'

Changelog:
 - 6c9b1dc218fe selftests: ovpn: serialize YNL listener startup
 - 1be93bb979ab selftests: ovpn: align command flow with TAP
 - 7c29665a3a3c selftests: ovpn: add prefix to helpers and shared
   variables
 - 222e7f8d1ca3 selftests: ovpn: flatten slurped notification JSON
   before filtering
 - c409da0fe15e selftests: ovpn: fail notification check on mismatch
 - e5fd34ab8dff selftests: ovpn: add nftables config dependencies for
   test-mark
 - 29ae61b2fe7e drivers: net: drop ipv6_stub usage and use direct
   function calls

Signed-off-by: Ralf Lici <ralf@mandelbit.com>

compat-include/net/ip6_route.h
@@ -20,7 +20,9 @@ ovpn_ipv6_dst_lookup_flow(struct net *net, const struct sock *sk,
 			  struct flowi6 *fl6,
 			  const struct in6_addr *final_dst)
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 119) || RHEL_RELEASE_CODE != 0
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(7, 1, 0)
+	return ip6_dst_lookup_flow(net, sk, &fl, final_dst);
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 119) || RHEL_RELEASE_CODE != 0
 	return ipv6_stub->ipv6_dst_lookup_flow(net, sk, fl6, final_dst);
 #else
 	struct dst_entry *entry;

backports-ctl.sh
@@ -3,7 +3,7 @@
 set -e
 
 KERNEL_REPO_URL='https://github.com/OpenVPN/ovpn-net-next.git'
-KERNEL_COMMIT=${KERNEL_COMMIT:-'ca7e99335aea7c5977683624ba319157a4603f96'}
+KERNEL_COMMIT=${KERNEL_COMMIT:-'98878ed91b68a3150126fccef125ee7b1bb86ab2'}
 KERNEL_DIR="$PWD/kernel"
 
 get_ovpn() {

compat-patches/sources/peer.c.patch
@@ -1,5 +1,5 @@
 diff --git a/drivers/net/ovpn/peer.c b/drivers/net/ovpn/peer.c
-index 26b55d8..8ff61fe 100644
+index c02dfab..8ff61fe 100644
 --- a/drivers/net/ovpn/peer.c
 +++ b/drivers/net/ovpn/peer.c
 @@ -416,7 +416,7 @@ static __be32 ovpn_nexthop_from_skb4(struct sk_buff *skb)
@@ -20,12 +20,11 @@ index 26b55d8..8ff61fe 100644
  		goto out;
  
  	dest = rt->rt_gw4;
-@@ -827,8 +827,7 @@ static struct in6_addr ovpn_nexthop_from_rt6(struct ovpn_priv *ovpn,
+@@ -827,7 +827,7 @@ static struct in6_addr ovpn_nexthop_from_rt6(struct ovpn_priv *ovpn,
  		.daddr = dest,
  	};
  
--	entry = ipv6_stub->ipv6_dst_lookup_flow(dev_net(ovpn->dev), NULL, &fl,
--						NULL);
+-	entry = ip6_dst_lookup_flow(dev_net(ovpn->dev), NULL, &fl, NULL);
 +	entry = ovpn_ipv6_dst_lookup_flow(dev_net(ovpn->dev), NULL, &fl, NULL);
  	if (IS_ERR(entry)) {
  		net_dbg_ratelimited("%s: no route to host %pI6c\n",

compat-patches/sources/udp.c.patch
@@ -1,18 +1,16 @@
 diff --git a/drivers/net/ovpn/udp.c b/drivers/net/ovpn/udp.c
-index 272b535..2949e26 100644
+index 059e896..2949e26 100644
 --- a/drivers/net/ovpn/udp.c
 +++ b/drivers/net/ovpn/udp.c
-@@ -13,8 +13,8 @@
+@@ -13,6 +13,7 @@
  #include <linux/udp.h>
  #include <net/addrconf.h>
  #include <net/dst_cache.h>
 +#include <net/ip6_route.h>
  #include <net/route.h>
--#include <net/ipv6_stubs.h>
  #include <net/transp_v6.h>
  #include <net/udp.h>
- #include <net/udp_tunnel.h>
-@@ -197,9 +197,9 @@ static int ovpn_udp4_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
+@@ -196,9 +197,9 @@ static int ovpn_udp4_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
  	dst_cache_set_ip4(cache, &rt->dst, fl.saddr);
  
  transmit:
@@ -25,16 +23,16 @@ index 272b535..2949e26 100644
  	ret = 0;
  err:
  	local_bh_enable();
-@@ -251,7 +251,7 @@ static int ovpn_udp6_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
+@@ -250,7 +251,7 @@ static int ovpn_udp6_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
  		dst_cache_reset(cache);
  	}
  
--	dst = ipv6_stub->ipv6_dst_lookup_flow(sock_net(sk), sk, &fl, NULL);
+-	dst = ip6_dst_lookup_flow(sock_net(sk), sk, &fl, NULL);
 +	dst = ovpn_ipv6_dst_lookup_flow(sock_net(sk), sk, &fl, NULL);
  	if (IS_ERR(dst)) {
  		ret = PTR_ERR(dst);
  		net_dbg_ratelimited("%s: no route to host %pISpc: %d\n",
-@@ -272,9 +272,9 @@ static int ovpn_udp6_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
+@@ -271,9 +272,9 @@ static int ovpn_udp6_output(struct ovpn_peer *peer, struct ovpn_bind *bind,
  	 * udp_tunnel_xmit_skb()
  	 */
  	skb->ignore_df = 1;
@@ -47,7 +45,7 @@ index 272b535..2949e26 100644
  	ret = 0;
  err:
  	local_bh_enable();
-@@ -437,12 +437,12 @@ void ovpn_udp_socket_detach(struct ovpn_socket *ovpn_sock)
+@@ -436,12 +437,12 @@ void ovpn_udp_socket_detach(struct ovpn_socket *ovpn_sock)
  	struct sock *sk = ovpn_sock->sk;
  
  	/* Re-enable multicast loopback */

commit 324be3f248123770f6bcbcd99133d15006e749dd

pre-v7.1: fix IPv6 dst lookup compat wrapper
The direct ip6_dst_lookup_flow path passes an undefined local variable
instead of the flowi6 pointer received by the compat wrapper.

Pass fl6 through like the other branches do.

Fixes: 6c4d515ace75 ("Update ovpn-net-next to 98878ed91b68")
Reported-by: Antonio Quartulli <antonio@openvpn.net>
Signed-off-by: Ralf Lici <ralf@mandelbit.com>

compat-include/net/ip6_route.h
@@ -21,7 +21,7 @@ ovpn_ipv6_dst_lookup_flow(struct net *net, const struct sock *sk,
 			  const struct in6_addr *final_dst)
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(7, 1, 0)
-	return ip6_dst_lookup_flow(net, sk, &fl, final_dst);
+	return ip6_dst_lookup_flow(net, sk, fl6, final_dst);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 119) || RHEL_RELEASE_CODE != 0
 	return ipv6_stub->ipv6_dst_lookup_flow(net, sk, fl6, final_dst);
 #else